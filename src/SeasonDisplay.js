4b38d9a081f9dc6309aacd1435ed0f9a25bde878
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2020-01-14 18:59:27 +0700

    Add icon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './SeasonDisplay.css';
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -11,10 +12,13 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
   const text = season === "summer" ? "Let's hit the beach" : "Burr, it's cold";
+  const iconName = season === "summer" ? "sun" : "snowflake";
 
   return (
-    <div>
-      <div>{text}</div>
+    <div className={`season-display ${season}` }>
+      <i className={`${iconName} icon massive icon-left-top`}></i>
+      <h1>{text}</h1>
+      <i className={`${iconName} icon massive icon-right-bottom`}></i>
     </div>
   );
 };
